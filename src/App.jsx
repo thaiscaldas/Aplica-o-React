502b0739e445081f939f26e77422e753cf58a8f9
Thais_Caldas <caldasthais097@gmail.com>
2023-10-24 12:48:02 -0300

    conceito de componentes e propriedades

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Post } from './Post';
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author="Thais Gonçalves"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ullam asperiores omnis assumenda modi laborum aut sapiente nostrum, placeat laboriosam, doloremque a dolore alias nihil incidunt vitae velit maxime in."
+      />
+      <Post 
+        author="May Martins"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ullam asperiores omnis assumenda modi laborum aut sapiente nostrum, placeat laboriosam, doloremque a dolore alias nihil incidunt vitae velit maxime in."
+      />
+
+    </div>
+  )
+}
+
+
