84f786f1f5976d4de6065a2e4676b997d4681a33
Thais Caldas <caldasthais097@gmail.com>
2023-10-25 01:01:04 -0300

    estilização com CSS Modules e criação de Estilos Globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post 
         author="Thais Gonçalves"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ullam asperiores omnis assumenda modi laborum aut sapiente nostrum, placeat laboriosam, doloremque a dolore alias nihil incidunt vitae velit maxime in."
