d2d6ba0041e1afe0be54b33a9be484bf7b69afad
Thais Caldas <caldasthais097@gmail.com>
2023-10-26 09:33:22 -0300

    concluindo Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,12 +9,14 @@ export function Sidebar() {
                 src="https://www.campuscameta.ufpa.br/images/logo_labex.jpg" 
             />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/thaiscaldas.png" />
                 <strong>Thais Gonçalves</strong>
                 <span>Programadora</span>
             </div>
 
             <footer>
                 <a href='#'>
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
